9622c88169e4260ba5ae005027f7b9c5f4d4cf5e
aodom3 <aodom3@gmail.com>
2018-06-08 14:56:39 -0400

    changed models folders around

---
+++ db/shopperSchema.js
@@ -0,0 +1,36 @@
+const Schema = require('mongoose').Schema
+// mongoose.connect('mongodb://localhost/shopper')
+
+// const db = mongoose.connection;
+
+// db.on('error', function(err) {
+//     console.log(err);
+//   });
+//   db.once('open', function() {
+//     console.log("database has been connected!");
+//   });
+  
+//   db.close();
+  
+const DrinkSchema = new Schema({
+    name: "string",
+    type: "string",
+    size: "number",
+    price: "number",
+    picture: "string",
+    stocked: "boolean"
+});
+
+const ShopperSchema = new Schema({
+    name: "string",
+    email: "string",
+    age: "number",
+    drink: [],
+    payment: "string",    
+});
+
+// const DrinkModel = mongoose.model("Drink", drinkSchema);
+// const ShopperModel = mongoose.model("Shopper", shopperSchema);
+
+
+module.exports = ShopperSchema
