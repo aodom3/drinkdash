4a0e52b917c68a0d8e8d345eb93df96a4029f6b1
aodom3 <aodom3@gmail.com>
2018-06-10 00:31:40 -0400

    added nav bar via  bootstrap

--- drinkdash/app.js
+++ drinkdash/app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const expressHbs = require('express-handlebars');
 const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', expressHbs ({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
