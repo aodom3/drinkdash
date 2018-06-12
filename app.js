0b279afbd0ffcf84bc1d3223e728e5f0dce485e4
aodom3 <aodom3@gmail.com>
2018-06-12 12:43:44 -0400

    working with git

---
+++ app.js
@@ -0,0 +1,67 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressHbs = require('express-handlebars');
+const router = require('./controllers/index');
+const mongoose = require('mongoose');
+const app = express();
+const methodOverride = require('method-override');
+
+const drinkController = require('./controllers/drinkController.js')
+app.use('/', drinkController)
+const cartController = require('./controllers/cartController')
+app.use('/cart', cartController)
+
+
+
+const handlebars = require('express-handlebars').create({
+  layoutsDir: path.join(__dirname, "views"),
+  partialsDir: path.join(__dirname, "views/partials"),
+  defaultLayout: 'layout',
+  extname: 'hbs'
+});
+
+
+
+mongoose.connect('mongodb://localhost/drinkdash')
+  .then(() => {
+    console.log('connected to mongoDB')
+  })
+  .catch((err) => {
+    console.log('ERROR', err)
+  })
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', handlebars.engine)
+app.use(methodOverride('_method'))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
